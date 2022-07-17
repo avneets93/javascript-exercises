d8a7d31a924215e2cd3816c46726ca36972b2309
Avneet Singh <avneets93@gmail.com>
2022-07-17 14:31:24 -0400

    Finish 9th exercise!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replaceAll(' ','');
+    str = str.replaceAll(',','');
+    str = str.replaceAll('.','');
+    str = str.replaceAll('!','');
 
+    let strArr = str.split('');
+    let reverseArr = [];
+    for(let i=0;i<strArr.length;i++){
+        reverseArr[strArr.length-i-1] = strArr[i];
+    }
+    let reverseStr = reverseArr.join('');
+    if (str.toUpperCase() === reverseStr.toUpperCase()) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
