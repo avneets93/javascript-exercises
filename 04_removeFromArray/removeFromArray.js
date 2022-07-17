6a72cf7d71499831f05c83ab9de3b7fcefa66b21
Avneet Singh <avneets93@gmail.com>
2022-07-16 23:00:00 -0400

    Finish 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray,...removeList) {
+    for (let i=0; i<inputArray.length; i++){
+        for (let j=0; j<removeList.length;j++){
+            if (inputArray[i] === removeList[j]){
+                inputArray.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
