deb1f2298511e5b5cbffe259a2605b6657d4b746
Avneet Singh <avneets93@gmail.com>
2022-07-16 23:36:57 -0400

    Finish 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  // [°C] = ([°F] − 32) × 5⁄9;
+  let tempCels = (temp - 32) * 5/9;
+  return Math.round(tempCels*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // [°F] = [°C] × 9⁄5 + 32;
+  let tempFarh = (temp*9/5) + 32;
+  return Math.round(tempFarh*10)/10;
 };
 
 // Do not edit below this line
