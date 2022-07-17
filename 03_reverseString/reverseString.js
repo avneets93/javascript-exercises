5de435ef135e4db4c16b4b760ae4569fc2999c95
Avneet Singh <avneets93@gmail.com>
2022-07-16 20:21:57 -0400

    Finish 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let tempArr = str.split('');
+    const len = tempArr.length;
+    let resultArr = [];
+    for (let i =0; i< len;i++){
+        resultArr[len-i-1] = tempArr[i];
+    }
+    return resultArr.join('');
 };
 
 // Do not edit below this line
