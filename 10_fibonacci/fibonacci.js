caaf95271ddeabfea00a6001ad041687f7242024
Avneet Singh <avneets93@gmail.com>
2022-07-17 18:03:49 -0400

    Finish 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index<0)return "OOPS";
+    else
+    {
+        let num1 = 1;
+        let num2 = 1;
+        let temp = 0;
+        for (let i=3;i<=index;i++) {
+            temp = num2;
+            num2 = num1 + num2;
+            num1 = temp;
+        }
+        return num2;
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
