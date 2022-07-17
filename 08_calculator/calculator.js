27671f8ec3430cf090f9323aa08332f1625f9306
Avneet Singh <avneets93@gmail.com>
2022-07-17 14:07:37 -0400

    Finish 8th exercise!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(const i of arr){
+    sum += i;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiply = 1;
+  for(const i of arr){
+    multiply *= i;
+  }
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = 1;
+  for(let i = 1; i<= b; i++){
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num ===0 || num ===1) return 1;
+  else{
+    let fact = 1;
+    for (let i=1; i<=num; i++){
+      fact *= i; 
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
