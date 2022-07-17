0b000256120e750e8b8dacf3852ba5264ad1e71d
Avneet Singh <avneets93@gmail.com>
2022-07-16 23:14:30 -0400

    Finisg 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(first,last) {
+    if (typeof first !== "number" || typeof last !== "number" || first <0 
+            || last <0){
+                return "ERROR";
+            } 
+    let small, large;
+    if (first<last) {
+        small = first;
+        large = last;
+    }
+    else {
+        small = last;
+        large = first;
+    }
+    let sum = 0;
+    for (let i=small;i<=large;i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
