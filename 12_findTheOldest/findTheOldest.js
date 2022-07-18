1418164765ae051aab783315ae1894c67d216d8c
Avneet Singh <avneets93@gmail.com>
2022-07-17 22:57:15 -0400

    Finish 12th exercise!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let age = 0;
+    let oldest = 0;
+    let oldestPerson = {};
+    const year = new Date();
+    for(const person of people){
+        if (person["yearOfDeath"]) age = person["yearOfDeath"] - person["yearOfBirth"];
+        else age = year.getFullYear() - person["yearOfBirth"];
+        if (age > oldest) {
+            oldest = age;
+            oldestPerson = person;
+        }    
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
